2df535734e0eaddfb19255184e595e60e082ccb7
Treveshan Naidoo <treveshan.naidoo@chillisoft.co.za>
2017-02-23 11:38:46 +0200

    Removed mock-heroes.ts added CRUD functions and get data from API

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -33,6 +33,12 @@ export class HeroDetailComponent implements OnInit {
     goBack(): void {
         this.location.back();
     }
+    
+    save(): void {
+  this.heroService.update(this.hero)
+    .then(() => this.goBack());
+}
+
 
     @Input()
     hero: Hero;
